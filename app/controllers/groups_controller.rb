718d1ad4cff6db537c33988b777db56317992565
activeliang <liang@activeliangdeiMac.local>
2017-01-03 15:52:14 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,10 +20,14 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params[:id])
-    @group.save
+
+    if @group.save
+
 
       redirect_to groups_path
-
+    else
+      render :new
+    end
   end
 
   def update
@@ -40,7 +44,7 @@ class GroupsController < ApplicationController
     flash[:alert] = "Group deleted"
 
     redirect_to groups_path
-    
+
   end
 
   private
